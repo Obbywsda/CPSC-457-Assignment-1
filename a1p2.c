de8d9a3fce9eee1dfd995c1a15cfa3b69a0826e2
CroyDobson <croy.dobson@ucalgary.ca>
2025-09-13 12:48:34 -0600

    added lines to read input

---
+++ a1p2.c
@@ -0,0 +1,11 @@
+#include <sys/types.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+    int lower = atoi(argv[1]);
+    int upper = atoi(argv[2]);
+    int N = atoi(argv[3]);
+    exit(0);
+}
