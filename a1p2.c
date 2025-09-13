4461a67725c2765b09be99af7b152ccfb72edd22
CroyDobson <croy.dobson@ucalgary.ca>
2025-09-13 14:11:22 -0600

    entered function to check if is prime

--- a1p2.c
+++ a1p2.c
@@ -3,9 +3,19 @@
 #include <sys/wait.h>
 #include <stdlib.h>
 
+int is_prime(int num);
+
 int main(int argc, char *argv[]){
     int lower = atoi(argv[1]);
     int upper = atoi(argv[2]);
     int N = atoi(argv[3]);
     exit(0);
+}
+
+int is_prime(int num) {
+    if (num < 2) return 0;
+        for (int i = 2; i <= sqrt(num); i++) {
+            if (num % i == 0) return 0;
+        }
+    return 1;
 }
