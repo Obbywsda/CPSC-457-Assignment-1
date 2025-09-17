5f450e32f7bd164d29e4a094966373e828acbe31
CroyDobson <croy.dobson@ucalgary.ca>
2025-09-16 18:29:51 -0600

    Added for loop and worked on logic for loop

--- a1p2.c
+++ a1p2.c
@@ -9,6 +9,44 @@ int main(int argc, char *argv[]){
     int lower = atoi(argv[1]);
     int upper = atoi(argv[2]);
     int N = atoi(argv[3]);
+    
+    if ((upper-lower)+1 <N)
+    {
+        N=(upper-lower)+1;
+        
+    }
+    double c = ((upper-lower)+1)/N;
+    int y = lower;
+    int i;
+    pid_t pid;
+    for (i = 0; i < N; i++)
+    {
+        
+        pid = fork();
+        if (pid < 0) {
+            printf("Fork Failed");
+            exit(-1);
+        }
+        else if (pid == 0) {
+            break;
+        }
+        else {
+            y+=(((int)(c*(i+1)-((int)(c*i)))));
+
+        }
+    }
+
+    if (pid==0)
+    {
+        
+    }
+    
+    
+    
+    
+
+
+    
     exit(0);
 }
 
